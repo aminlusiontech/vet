1caa732d0c7fa3bd5ca90c08c9960acbe38067d5
hasham usman <linkbyhasham1174@gmail.com>
2026-03-03 00:07:06 +0500

    feat: age verification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -117,7 +117,7 @@ import AdminDashboardStaff from "./pages/AdminDashboardStaff";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
-import AgeVerification from "./components/Layout/AgeVerification";
+// import AgeVerification from "./components/Layout/AgeVerification";
 import { loadUser } from "./redux/actions/user";
 import { loadAdmin } from "./redux/actions/admin";
 import ProtectedRoute from "./routes/ProtectedRoute";
@@ -736,7 +736,7 @@ const App = () => {
     <ErrorBoundary>
       <HashRouter>
         <ScrollToTop />
-        <AgeVerification />
+        {/* <AgeVerification /> */}
         <NotificationProvider>{routes}</NotificationProvider>
         <ToastContainer
           position="bottom-center"
